d0a757d89b88bed765c6f172df88dfc034709a0d
tkskto <tkskto@gmail.com>
2021-05-17 21:27:29 +0900

    心理的安全人間になる

--- static/blog/sw.js
+++ static/blog/sw.js
@@ -1,13 +1,17 @@
-const CACHE_KEY = 'tkskto.me.blog';
+const CACHE_KEY = '1.0.1';
+const CACHE_KEY_OLD = 'tkskto.me.blog';
 const urlsToCache = [
     '/blog/common/css/app.css',
     '/blog/common/css/vendors/app.css',
-    '/blog/common/css/pages/blog/index.css',
-    '/blog/index.html',
 ];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_KEY).then((cache) => cache.addAll(urlsToCache)));
+    e.waitUntil(
+        Promise.all([
+            caches.open(CACHE_KEY_OLD).then((cache) => cache.delete(CACHE_KEY_OLD)),
+            caches.open(CACHE_KEY).then((cache) => cache.addAll(urlsToCache))
+        ])
+    );
 });
 
 self.addEventListener('fetch', (e) => {
