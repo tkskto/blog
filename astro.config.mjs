5ab886b6ae19f3d507b0272db3cec1741a8671ce
tkskto <tkskto@gmail.com>
2025-05-11 14:21:07 +0900

    fix: サイトマップのドメイン修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tkskto.me',
+    site: 'https://blog.tkskto.me',
     outDir: './dist',
     trailingSlash: 'always',
     integrations: [
